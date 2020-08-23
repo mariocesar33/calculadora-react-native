e96b75ee29fe7e479032d33a0c424fddf1ac1e87
Mário César Silva <mcgato33@hotmail.com>
2020-08-23 19:45:23 -0100

    interface concluida

---
+++ src/components/Button.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableHighlight
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    fontSize: 40,
+    height: Dimensions.get('window').width / 4,
+    width: Dimensions.get('window').width / 4,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+  },
+
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#2e86c0',
+  },
+
+  clearButton: {
+    color: '#fff',
+    backgroundColor: '#f0595f',
+  },
+
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  }
+});
+
+export default props => {
+
+  const stylesButton = [styles.button];
+
+  if (props.double) stylesButton.push(styles.buttonDouble);
+
+  if (props.clear) stylesButton.push(styles.clearButton);
+
+  if (props.operation) stylesButton.push(styles.operationButton);
+
+  return (
+    <TouchableHighlight onPress={props.onClick}>
+      <Text style={stylesButton}>{props.label}</Text>
+    </TouchableHighlight>
+  );
+}
