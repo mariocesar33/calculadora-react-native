7875758a98c290e78591474a41e14238df496511
Mário César Silva <mcgato33@hotmail.com>
2020-08-25 06:14:34 -0100

    logica da calculadora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Button from '../src/components/Button';
 import Display from '../src/components/Display';
 
-class App extends Component {
-  state = {
-    displayValue: '0'
-  }
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
 
-  addDigit = n => {
-    this.setState({ displayValue: n});
+class App extends Component {
+  state = { ...initialState }
+
+  addDigit = caractere => { // para não ter um numero com dois '.' exemplo -> 25.6.7.
+    if (caractere === '.' && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+
+//para não termos zero a esquerda de um numero ex.(08)     para não ter uma sequencia de zeros ex.(00000)        
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + caractere ; //concatenar numeros
+
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (caractere !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+
+      values[this.state.current] = newValue;
+      this.setState({ values});
+    }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: '0' });
+    this.setState({ ...initialState });
   }
 
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals =  operation === '=';
+      const values = [...this.state.values];
 
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   }
 
   render() {
